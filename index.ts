479973ee7815756dd4253a8ea4a1794e82ed2620
Caique Sobral <caiquelsobral@gmail.com>
2025-02-21 13:01:13 -0300

    feat: add tailwind

--- index.ts
+++ index.ts
@@ -1,4 +1,5 @@
-import { registerRootComponent } from "expo";
-import App from "@/App";
+import { registerRootComponent } from 'expo'
+import './src/global.css'
+import App from '@/App'
 
-registerRootComponent(App);
+registerRootComponent(App)
